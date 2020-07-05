9d19cccff86280156a8a3a5efb8b60fc6f76203b
lwei20000 <1291958264@qq.com>
2020-07-05 23:05:18 +0800

    提交新文件

--- 02-Order/src/main/java/com/itcast/OrderApplication.java
+++ 02-Order/src/main/java/com/itcast/OrderApplication.java
@@ -1,13 +1,10 @@
 package com.itcast;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
